f87a1edeb12865164aac8cdcc491dd624ff7f88a
Miguel Grilo <miguel.ptfg@hotmail.com>
2020-03-12 22:36:20 +0000

    server accepts multiple clients

--- Client_API/src/main/java/Client_API/App.java
+++ Client_API/src/main/java/Client_API/App.java
@@ -1,29 +1,41 @@
 package Client_API;
 
-import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import java.util.Scanner; 
 
 
-public class App 
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World from the Client_API!" );
+public class App {
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner 
-        System.out.println("Enter a message:");
-        String message = myObj.nextLine();  // Read user input
-        myObj.close();
-        
+	private static Scanner myObj;
+	private static ObjectOutputStream outStream;
+	private static ObjectInputStream inStream;
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World from the Client_API!" );        
         try{      
-            Socket s = new Socket("localhost",9000);  
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());  
-            dout.writeUTF(message);  
-            dout.flush();  
-            System.out.println("Message sent!");  
-            dout.close();  
-            s.close();  
+            Socket socket = new Socket("localhost",9000);
+            // Instantiate input and output streams
+            outStream = new ObjectOutputStream(socket.getOutputStream());
+            inStream = new ObjectInputStream(socket.getInputStream());
+            // Instantiate scanner
+            myObj = new Scanner(System.in);
+            // Control variable
+            Boolean open = true;
+            while(open) {
+                System.out.println("Enter a message:");
+                // Read usewr input
+                String message = myObj.nextLine();
+                outStream.writeObject(message);
+                System.out.println("Message sent!");
+                if(message.equals("END")) {
+                	open = false;
+                }
+            }
+            System.out.println("Bye!");
+            socket.close();  
         } catch(Exception e){
             System.out.println(e);
         }  
--- Server/src/main/java/Server/App.java
+++ Server/src/main/java/Server/App.java
@@ -1,34 +1,76 @@
 package Server;
 
-import java.io.DataInputStream;
-
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World from server!" );
 
-        try{  
-            ServerSocket ss = new ServerSocket(9000);
-            Boolean open = true;
-
-            while (open){
-                Socket s = ss.accept();
-                DataInputStream dis = new DataInputStream(s.getInputStream());  
-                String str = (String)dis.readUTF();
-                System.out.println("message = "+str);
-
-                if (str.equals("END")) {
-                    open = false;
-                }
+        try{
+        	// Start server
+        	ServerSocket ss = new ServerSocket(9000);
+            System.out.println("Server running on port 9000!");
+            // Accept clients and open threads for each one of them
+            while (true){
+                Socket inSocket = ss.accept();
+                ServerThread newServerThread = new ServerThread(inSocket);
+				newServerThread.start();
             }
-
-            ss.close();
-                        
+            // ss.close(); agora o cliente so fecha a sua conexao... por enquanto para mandar o server abaixo é mandar um CTRL+C         
         } catch(Exception e) {
             System.out.println("Something went wrong.");
         }  
+    }
+    
+    // Class that is instantiated when a user connects to the server
+    static class ServerThread extends Thread {
+
+    	private Socket socket = null;
+    	ObjectOutputStream outStream;
+    	ObjectInputStream inStream;
+
+    	public ServerThread(Socket clientSocket) {
+    		this.socket = clientSocket;
+    	}
+
+    	public void run() {
+
+    		try {
+    			outStream = new ObjectOutputStream(socket.getOutputStream());
+    			inStream = new ObjectInputStream(socket.getInputStream());
+    			String cmd = null;
+    			Boolean open = true;
+    			try {
+    				System.out.println("User connected.");
+    				while(open) {
+    					cmd = (String) inStream.readObject();
+    					switch(cmd) {
+    						case "POST":
+    							post();
+    							break;
+    						case "END":
+    							quitUser();
+    							open = false;
+    							break;
+    					}
+    				}
+    			}catch (Exception e) {
+    				e.printStackTrace();
+    			}
+    		}catch (Exception e) {
+    			e.printStackTrace();
+    		}
+    	}
+
+		private void post() {
+			System.out.println("POST");
+		}
+		
+		private void quitUser() {
+			System.out.println("User disconnected.");
+		}
     }
 }
