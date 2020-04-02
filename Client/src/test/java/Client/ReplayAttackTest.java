8832047fc9b15b22411f539e6eac72691bbc151e
manuelmanso <miguel.francisco1998@gmail.com>
2020-04-02 22:43:44 +0100

    aiai

--- Client/src/test/java/Client/ReplayAttackTest.java
+++ Client/src/test/java/Client/ReplayAttackTest.java
@@ -12,7 +12,8 @@ public class ReplayAttackTest extends BaseTest {
     public static void populate() throws AlreadyRegisteredException, UnknownPublicKeyException, NonceTimeoutException, OperationTimeoutException, FreshnessException, IntegrityException, UserNotRegisteredException, InvalidAnnouncementException, MessageTooBigException {
         clientEndpoint1.register();
         clientEndpoint1.post("USER1 MESSAGE1", null);
-        clientEndpoint1.postGeneral("USER1 MESSAGE2", null);
+
+        //clientEndpoint1.postGeneral("USER1 MESSAGE3", null);
     }
 
     /**
@@ -21,11 +22,12 @@ public class ReplayAttackTest extends BaseTest {
 
     @Test(expected = TooMuchAnnouncementsException.class)
     public void Should_not_Post_More_Than_One() throws MessageTooBigException, InvalidAnnouncementException, NonceTimeoutException, FreshnessException, UserNotRegisteredException, IntegrityException, OperationTimeoutException, TooMuchAnnouncementsException, InvalidPostsNumberException {
+
         clientEndpoint1.setReplay_flag(true);
 
         clientEndpoint1.post("this message will not be repeated", null);
 
-        clientEndpoint1.read("user1", 4); //se o replay for bem sucedido, ha 4 posts no server
+        clientEndpoint1.read("user1", 3); //se o replay for bem sucedido, ha 3 posts no server
     }
 
     @Test(expected = TooMuchAnnouncementsException.class)
@@ -34,7 +36,7 @@ public class ReplayAttackTest extends BaseTest {
 
         clientEndpoint1.postGeneral("this message will not be repeated", null);
 
-        clientEndpoint1.readGeneral(4); //se o replay for bem sucedido, ha 4 posts no server
+        clientEndpoint1.readGeneral(4); //se o replay for bem sucedido, ha 3 posts no server
     }
 
     @Test
@@ -50,12 +52,18 @@ public class ReplayAttackTest extends BaseTest {
     }
 
     @Test
-    public void Should_not_Read_More_Than_One_General() throws UserNotRegisteredException, NonceTimeoutException, OperationTimeoutException, InvalidPostsNumberException, IntegrityException, TooMuchAnnouncementsException, FreshnessException {
+    public void Should_not_Read_More_Than_One_General() throws UserNotRegisteredException, NonceTimeoutException, OperationTimeoutException, InvalidPostsNumberException, IntegrityException, TooMuchAnnouncementsException, FreshnessException, MessageTooBigException, InvalidAnnouncementException {
+
+        clientEndpoint1.postGeneral("USER1 MESSAGE2", null);
+
         clientEndpoint1.setReplay_flag(true);
+
+        clientEndpoint1.postGeneral("USER1 MESSAGE3", null);
+
 
         String[] result1 = getMessagesFromJSON(clientEndpoint1.readGeneral(2));
 
-        assertEquals(result1[0], "this message will not be repeated");
+        assertEquals(result1[0], "USER1 MESSAGE3");
         assertEquals(result1[1], "USER1 MESSAGE2");
         assertEquals(result1.length, 2);
     }
