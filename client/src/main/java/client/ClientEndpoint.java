c7ead7c91335e931cd875204042560535de4ba22
badjelix <miguel.ptfg@hotmail.com>
2020-05-08 00:52:19 +0100

    add getpublickey and verifyresponse on server crypto

--- client/src/main/java/client/ClientEndpoint.java
+++ client/src/main/java/client/ClientEndpoint.java
@@ -20,7 +20,6 @@ import java.util.concurrent.ExecutionException;
 public class ClientEndpoint {
 
 	
-	private Map<PublicKey, byte[]> serversNonces = null;
 	private Map<PublicKey, byte[]> clientNonces  = null;
 	private Map<PublicKey, Integer> serversPorts = null;
 	
@@ -59,7 +58,6 @@ public class ClientEndpoint {
         setUsername(userName);
         serversPorts = initiateServersPorts();
 
-        serversNonces = new HashMap<PublicKey, byte[]>();
         clientNonces  = new HashMap<PublicKey, byte[]>();
     }
 
@@ -123,14 +121,6 @@ public class ClientEndpoint {
 
     public void setUsername(String userName) {
         this.userName = userName;
-    }
-
-    public byte[] getServerNonce(PublicKey serverKey) {
-        return serversNonces.get(serverKey);
-    }
-
-    public void setServerNonce(PublicKey serverKey, byte[] serverNonce) {
-        this.serversNonces.put(serverKey, serverNonce);
     }
 
     public byte[] getClientNonce(PublicKey serverKey) {
