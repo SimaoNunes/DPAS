ab647015feee5586abb7d47fb7d4b4349253f121
SimaoNunes <simaspedro1@gmail.com>
2020-05-08 02:48:37 +0100

    read complete checking exceptions and nonces

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -172,8 +172,13 @@ public class Server implements Runnable {
                         }
                         break;
                     case "READCOMPLETE":
-                        //checkar o nonce
-                        readComplete(envelope.getRequest());
+                        if(checkExceptions(envelope.getRequest(), outStream, new int[] {-7}) &&
+                            cryptoManager.verifyRequest(envelope.getRequest(), envelope.getSignature()) &&
+                            cryptoManager.checkNonce(envelope.getRequest().getPublicKey(), envelope.getRequest().getServerNonce()) &&
+                            checkExceptions(envelope.getRequest(), outStream, new int[] {-3}))
+                            {
+                            readComplete(envelope.getRequest());
+                        }
                     case "NONCE":
                         handshake = true;
                         byte[] randomNonce = cryptoManager.generateRandomNonce(envelope.getRequest().getPublicKey());
